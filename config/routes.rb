fcdeb75e7c1220cd82b53977131ecf3787833d81
Suziwan <christe.suzanne@gmail.com>
2023-03-07 18:54:47 +0000

    Add controllers, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "english#index"
+
+  get 'english/index'
+  get 'french/index'
+  get 'portuguese/index'
 end
