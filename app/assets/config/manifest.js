050bc2462beb21c07502962a620ee197ad001875
Suziwan <christe.suzanne@gmail.com>
2023-03-07 19:38:02 +0000

    Add audio_tag and manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,13 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link clap.wav
+//= link hihat.wav
+//= link kick.wav
+//= link openhat.wav
+//= link boom.wav
+//= link ride.wav
+//= link snare.wav
+//= link tom.wav
+//= link tink.wav
